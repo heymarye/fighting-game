4bb45fc47a941673dfdbd9ef8dc9ca527b53602f
Marija Morozova <mmoroz@ttu.ee>
2022-05-15 11:27:19 +0300

    characters are created

--- script.js
+++ script.js
@@ -7,3 +7,62 @@ canvas.width = 1024;
 canvas.height = 576;
 
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+const gravity = 0.2;
+
+class Sprite {
+    constructor({ position, velocity }) {
+        this.position = position;
+        this.velocity = velocity;
+        this.height = 150;
+    }
+
+    draw() {
+        context.fillStyle = 'red';
+        context.fillRect(this.position.x, this.position.y, 50, this.height);
+    }
+
+    update() {
+        this.draw();
+        this.position.y += this.velocity.y;
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0;
+        } 
+        else {
+            this.velocity.y += gravity;
+        }
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+});
+
+const enemy = new Sprite({
+    position: {
+        x: 974,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+});
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    player.update();
+    enemy.update();
+}
+
+animate();
